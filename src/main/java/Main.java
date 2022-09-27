ee20b430e2fdbe1eb0ebad7735be92336594cdc2
marcinlopatka <lopatkamarcin@o2.pl>
2022-09-27 22:50:08 +0200

    Update main java class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("World says Hello again!");
+        System.out.println("World says Hello again!!!");
     }
 }
