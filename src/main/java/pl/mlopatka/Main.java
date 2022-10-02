3b56ef06b266b77d7ace054e63756624ffe7a154
marcinlopatka <lopatkamarcin@o2.pl>
2022-10-02 12:43:33 +0200

    use name service

--- src/main/java/pl/mlopatka/Main.java
+++ src/main/java/pl/mlopatka/Main.java
@@ -7,9 +7,11 @@ public class Main {
 
     private static final TestService TEST_SERVICE = new TestService();
     private static final FeatureService SERVICE = new FeatureService();
+    private static final NameService NAME_SERVICE = new NameService();
 
     public static void main(String[] args) {
         System.out.println("Test status: " + TEST_SERVICE.testStatus());
         SERVICE.doSth();
+        System.out.println(NAME_SERVICE.generateName());
     }
 }
