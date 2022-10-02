093d993dedb22a7a5f0013a3926222e69c51553a
marcinlopatka <lopatkamarcin@o2.pl>
2022-10-02 12:46:21 +0200

    use name service

--- src/main/java/pl/mlopatka/NameService.java
+++ src/main/java/pl/mlopatka/NameService.java
@@ -1,8 +1,14 @@
 package pl.mlopatka;
 
+import java.util.List;
+import java.util.Random;
+
 public class NameService {
 
+    private static final List<String> NAMES = List.of("Marcin", "Tadeusz", "Jadwiga", "Matylda", "Amdaeusz");
+    private static final Random RANDOM = new Random();
+
     public String generateName() {
-        return "Marcin";
+        return NAMES.get(RANDOM.nextInt(5));
     }
 }
