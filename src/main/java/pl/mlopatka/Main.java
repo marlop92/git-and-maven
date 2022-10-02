c5aabdbce5e7b1a7127ba0bc2ea091efb25ef7d6
marcinlopatka <lopatkamarcin@o2.pl>
2022-10-02 12:47:44 +0200

    use test services

--- src/main/java/pl/mlopatka/Main.java
+++ src/main/java/pl/mlopatka/Main.java
@@ -5,9 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Main {
 
+    private static final TestService TEST_SERVICE = new TestService();
     private static final FeatureService SERVICE = new FeatureService();
 
     public static void main(String[] args) {
+        System.out.println("Test status: " + TEST_SERVICE.testStatus());
         SERVICE.doSth();
     }
 }
